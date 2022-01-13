04bbe656b58ef27f9b2ff0cfb6d64dad56ee0342
Madhavi Latha Yayavaram <lathayvm@gmail.com>
2022-01-13 17:14:13 +0530

    Finish

--- APITask2.js
+++ APITask2.js
@@ -1,21 +1,19 @@
-async function getMetaphor(paras,sentences) {
-    const data = await 
-    fetch(`http://metaphorpsum.com/paragraphs/${paras}/${sentences}` , { method:"GET" });
-    const description=await data.json();
-    console.log(JSON.stringify(description.data));
-    return description;
-  }
-  
-  
-    async function displayInfo() {
-      let paras = parseInt(document.getElementById('paras').value);
-      let sentences = parseInt(document.getElementById('sentences').value);
-     
-        console.log (paras, sentences);
-        
-    const wordinfo  = [] = await getMetaphor(paras,sentences);
-        console.log (wordinfo);
-       
-  
-  } 
+function getMetaphor(p,s) {
+  let metaphor = fetch(`http://metaphorpsum.com/paragraphs/${p}/${s}`)
+  .then(function(response) {
+    return response.text();
+  }).then(function(data) {
+    return data;
+  });
+  return metaphor;
+}
+  async function displayInfo() {
+    let paras = parseInt(document.getElementById('paras').value);
+    let sentences = parseInt(document.getElementById('sentences').value);
+   
+   
+  const wordinfo = await getMetaphor(paras,sentences);
+      console.log (wordinfo);
+     document.getElementById('metaphor').innerHTML = wordinfo;
 
+} 
